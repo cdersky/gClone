87e1f5754959beefbf733e9c083ec83d314eee89
Claire <cdersky@gmail.com>
2016-08-17 20:51:20 -0400

    Add ssh authentication

--- gc.sh
+++ gc.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # query user for repo name
-echo 'enter repo name (to clone)'
+echo 'enter repo name (to clone - do NOT use the .git extension)'
 
 # store user name in repoName variable
 read repoName
 
+# set up ssh to grab key for git authentication
 # clone repo into current directory
-# this works a particular user, in the example case cdersky
-git clone 'git@github.com:cdersky/'$repoName'.git'
+GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa' git clone 'git@git.movableink.com:'$repoName'.git'
