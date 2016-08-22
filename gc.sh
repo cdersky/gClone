5656aafbf5d794c8f60a6eb49fedd4d2e2389e70
Claire <cdersky@gmail.com>
2016-08-21 22:31:55 -0700

    Simplified repo selection
    now, just type in the number of the repo wanted. This avoids typos that lead to cloning empty repos.

--- gc.sh
+++ gc.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# query user for repo name
-echo 'enter repo name (to clone - do NOT use the .git extension)'
+# array of repos from https://github.com/movableink/resources/blob/master/docs/client-repos.md, last updated 18-Aug-2016
+repoList=("ace-hardware" "adt" "american-home-shield" "amex" "amex-ctn" "amex-international" "average-joe" "basspro" "beauty-boutique" "betfair" "betsson" "bhf" "bloomingdales" "boden" "brooks" "cancer-research-uk" "capital-one" "champs" "chanel" "choice-hotels" "clarks" "columbia" "comcast" "connections-education" "crackle" "dunkin-donuts" "darden" "delta" "disneystores" "dow-jones" "eastbay" "ebay" "eharmony" "emirates" "espn" "ezlinks" "fedex" "fossil" "fourseasons" "ft" "FootLocker" "fxcm" "gap" "gilt" "halfords" "hartford" "hilton" "history-channel" "hipmunk" "hof" "home-depot" "homedepotca" "hoteltonight" "hostelworld" "hsn" "jetblue" "joefresh" "lenovo" "macys" "mcdonalds" "medieval-times" "mrandmrssmith" "msg" "nascar" "news-america" "nickelodeon" "nyjets" "ny-times" "nypr" "office-depot" "orbitz" "paddypower" "palace" "palace-sports" "pearle-vision" "pinnacle" "publicis" "puma" "razor-gator" "saloncentric" "screwfix" "shopdirect" "shopping-channel" "siriusxm" "skybet" "sony" "spark44" "sprint" "starbucks" "stubhub" "td" "tesco" "thepalms" "ticketmaster" "ticketmaster-uk" "ticket-liquidator" "time" "trip-advisor" "totalwine" "uber" "virgin-america" "virgin-holiday" "warby-parker" "walgreens" "western-union" "rei")
 
-# store user name in repoName variable
-read repoName
+# print out list of repos and assign selected repo to variable repoName
+select repoName in ${repoList[@]}
+do
+	echo "repo selected is $repoName"
+	break
+done
 
-# set up ssh to grab key for git authentication
+
+# need to open the connection to your authentication agent
 # clone repo into current directory
 GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa' git clone 'git@git.movableink.com:'$repoName'.git'
